113ada1f405c67f54a1be2bb294879a921067bc6
Ashley Pliler <ashleypliler1@gmail.com>
2023-03-13 21:58:59 -0600

    first commit

---
+++ src/redux/slices/potentialCountriesSlice.js
@@ -0,0 +1,29 @@
+import {createSlice} from "@reduxjs/toolkit";
+
+const potentialCountriesSlice = createSlice({
+    name: 'potentialCountries',
+    initialState: {
+        value: [
+            {
+                name: {
+                    common: 'America'
+                },
+            },
+        ],
+    },
+    reducers: {
+        setPotentialCountries: (state, action) => {
+             state.value = action.payload;
+        },
+        deletePotentialContries: (state) => {
+             state.value = null;
+        },
+    },
+});
+
+export const {setPotentialCountries, deletePotentialContries } = 
+    potentialCountriesSlice.actions;
+
+export const selectPotentials = (state) => state.potentialCountries.value;
+
+export default potentialCountriesSlice.reducer;
